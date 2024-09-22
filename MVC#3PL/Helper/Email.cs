430b9ba4b29ab75721aaf065e8df91597613d160
MohaamedSaaied <mohamedelokr20@gmail.com.com>
2024-09-22 11:31:24 +0300

    MVC#6

---
+++ MVC#3PL/Helper/Email.cs
@@ -0,0 +1,10 @@
+﻿namespace MVC_3PL.Helper
+{
+    public class Email
+    {
+        public string Subject { get; set; }
+        public string Body { get; set; }
+        public string From { get; set; }
+        public string To { get; set; }
+    }
+}
